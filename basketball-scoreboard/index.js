ea5ab4a779ceb3a42db6a8ba43ad40c25d2c3fec
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2024-10-20 18:13:57 +0800

    Refactor basketball-scoreboard layout and add score increment functionality

---
+++ basketball-scoreboard/index.js
@@ -0,0 +1,14 @@
+function add1score() {
+  var score = document.getElementById('score1');
+  score.innerHTML = parseInt(score.innerHTML) + 1;
+}
+
+function add2score() {
+  var score = document.getElementById('score1');
+  score.innerHTML = parseInt(score.innerHTML) + 2;
+}
+
+function add3score() {
+  var score = document.getElementById('score1');
+  score.innerHTML = parseInt(score.innerHTML) + 3;
+}
