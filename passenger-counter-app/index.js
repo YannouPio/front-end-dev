188c1ef1f4b6d7ce3be60a398c8624c8a1289850
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2024-10-20 15:13:12 +0800

    Refactor passenger-counter-app layout, add increment and save functionality

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,15 +1,16 @@
-// document.getElementById("count-el").innerText = 5
-
-// change the count-el in the HTML to reflect the new count
-
-// camelCase
-let countEl = document.getElementById("count-el") // pass in arguments
-
-console.log(countEl)
-
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
