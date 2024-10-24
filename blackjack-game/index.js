0aa5702eace92ecf738486cb3969ba1f10943fd4
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2024-10-24 21:28:49 +0800

    Refactor blackjack game to use an array for cards and add new card functionality

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,39 +1,44 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let cards = [];
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function getRandomCard() {
+    return Math.floor(Math.random() * 11) + 1; // 生成 1 到 11 的随机数
+}
 
 function startGame() {
-    renderGame()
+    cards = [getRandomCard(), getRandomCard()]; // 随机生成初始两张卡
+    renderGame();
+}
+
+function calculateSum() {
+    return cards.reduce((total, card) => total + card, 0);
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+    let sum = calculateSum();
+    cardsEl.textContent = `Cards: ${cards.join(' ')}`;
+    sumEl.textContent = `Sum: ${sum}`;
+
+    if (sum < 21) {
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
     } else {
-        message = "You're out of the game!"
-        isAlive = false
+        message = "You're out of the game!";
+        isAlive = false;
     }
-    messageEl.textContent = message
+    messageEl.textContent = message;
 }
 
-
 function newCard() {
-    let card = 6
-    sum += card
-    // Push the card to the cards array
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    let card = getRandomCard();
+    cards.push(card);
+    console.log(cards);
+    renderGame();
 }
