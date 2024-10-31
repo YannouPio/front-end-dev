953a361fb49816ab22aa084e1ce0d14f9ee5890e
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2024-10-31 16:09:18 +0800

    Refactor lead rendering in Chrome extension to use template strings for improved readability

--- a-Chrome-Extension/index.js
+++ a-Chrome-Extension/index.js
@@ -1,18 +1,25 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    ulEl.innerHTML += "<li>" + inputEl.value + "</li>"
     inputEl.value = ""
+    renderLeads()
 })
 
-// function renderLeads() {
-//     let listItems = ""
-//     for (let i = 0; i < myLeads.length; i++) {
-//         listItems += "<li>" + myLeads[i] + "</li>"
-//     }
-//     ulEl.innerHTML = listItems
-// }
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Refactor the code below to use a template string
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
