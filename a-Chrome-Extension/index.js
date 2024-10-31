861489259310fe5bd09f4e87cd4f377b732456c1
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2024-10-31 14:57:32 +0800

    Refactor list item rendering in Chrome extension to directly update innerHTML on button click

--- a-Chrome-Extension/index.js
+++ a-Chrome-Extension/index.js
@@ -5,15 +5,14 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    ulEl.innerHTML += "<li>" + inputEl.value + "</li>"
+    inputEl.value = ""
 })
 
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-// Assign it to an empty string to begin with
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-    listItems += "<li>" + myLeads[i] + "</li>"
-}
-// 3. Render the listItems inside the unordered list using ulEl.innerHTML
-ulEl.innerHTML = listItems
+// function renderLeads() {
+//     let listItems = ""
+//     for (let i = 0; i < myLeads.length; i++) {
+//         listItems += "<li>" + myLeads[i] + "</li>"
+//     }
+//     ulEl.innerHTML = listItems
+// }
