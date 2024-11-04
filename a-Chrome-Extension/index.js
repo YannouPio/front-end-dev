6fcfca0b76b39fe3949544cc3985153606fcc461
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2024-11-04 22:36:21 +0800

    Store leads in localStorage and retrieve on initialization

--- a-Chrome-Extension/index.js
+++ a-Chrome-Extension/index.js
@@ -3,9 +3,16 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
